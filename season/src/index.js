9aeec25861c0557f9c35c3c00889f9be5034b9e9
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-04-28 11:27:18 +0400

    refactor to class react component

--- season/src/index.js
+++ season/src/index.js
@@ -2,12 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  //optional to define constructor if we have intial setup
+  constructor(props) {
+    // should call super to avoid error
+    super(props);
+    //only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: "" };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        // call back function it will run in future
+        this.setState({ lat: position.coords.latitude });
+      },
+
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitude:</div>;
+  }
+  //react should have render
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && !this.state.lat) {
+      return <div>Latitiude:{this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
