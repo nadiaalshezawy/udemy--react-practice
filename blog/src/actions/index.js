5a82609d860ced2719e229796f6ed9bc1fa4163b
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-06-20 16:21:16 +0400

    add user name to posts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,21 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  //console.log(getState().posts);
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  //refactoring usin lodash
+  /*
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+    */
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -11,3 +28,11 @@ export const fetchUser = (id) => async (dispatch) => {
   //console.log(response);
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+/*
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  //console.log(response);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
+*/
